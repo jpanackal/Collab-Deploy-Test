84f8df49fc47919fe923fdb5fbc4f9771c7a68ca
jpanackal <jpanackal@wisc.edu>
2022-03-25 19:24:45 -0500

    Server details

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const buddyList = require('spotify-buddylist')
 const app = express();
 const port = 5000;
+const path = require('path');
+const cors = require('cors');
+
 
 async function displayCollabActivity(req ,res) {
     const spDcCookie = 'AQCijEo-LRA31LztfGJeyOpRTUhjhwH-wD97XR2feFMvqRI2Fl_VPuXj5h5-sgrxgfXwNeI5ydgFQ3x2P8UTECuAdANJ0I7zH3JzWeh4-8kE8DO7MezwoFjikSYDQLVD5y6M0abup5Q4UDFZAD9wA_JuiUXx2Rsx'
@@ -11,32 +14,33 @@ async function displayCollabActivity(req ,res) {
   
     console.log(JSON.stringify(friendActivity.friends, null, 2))
     res.send(JSON.stringify(friendActivity.friends, null, 2));
-    // return friendActivity
-    // for(var i = 0; i < friendActivity.friends.length; i++) {
-    // var membersOnlyJson = new Array();
-    //   var obj = friendActivity.friends[i];
-    //   if(collabSpotifyNames.includes(obj.user.name)) {
-    //     console.log("Member: " + obj.user.name)
-    //     console.log("Member Image: " + obj.user.imageUrl)
-    //     console.log("Song: " + obj.track.name)
-    //     console.log("Song Image: " + obj.track.imageUrl)
-    //     console.log("Artist: " + obj.track.artist.name)
-    //     console.log("Album/Playlist: " + obj.track.context.name)
-    //     console.log("--------------------------------------")
-    //   }
-    // }
   }
 
   app.get("/api", displayCollabActivity);
 
-// app.get("/api", (req, res) => {
-//     // let example = displayCollabActivity();
+// ** MIDDLEWARE ** CHANGE THE THIRD ELEMENT IN THE AREA //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://shrouded-journey-38552.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
-//     // console.log(example);
-//     console.log(req)
-//     // res.send("hello")
-//     res.json({"users": ["user 1", "users 2", "user 397"] })
-//     // res.json(example)
-// })
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(5000, () => { console.log("server started on port 5000") })
